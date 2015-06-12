d0aea96835d656fc8bf62e0773f258bdf3504960
Cameron Fisk <cfisk@morristechnology.com>
2015-06-12 14:37:24 -0400

    sol-4434 created menu widget

--- client/views/common/sidebar.js
+++ client/views/common/sidebar.js
@@ -5,9 +5,10 @@ Template.sidebar.helpers({
 
 Template.sidebar.onRendered(function () {
     $('#menu_button').click(function() {
-        $('#menu').sidebar('setting', {dimPage: false});
+        $('#menu').sidebar('setting', {dimPage: true});
         $('#menu').sidebar('toggle');
         console.log($('.ui.dropdown'));
         $('.ui.dropdown').dropdown();
     });
 });
+
